cd589a13a91cc2aa2be2eb206c24c8a1c4968ae1
weizhenhua-deepin <weizhenhua94@163.com>
2019-03-08 20:38:36 +0800

    1. test module can run, add some date into mysql is ok.

--- Movie_project/app/models.py
+++ Movie_project/app/models.py
@@ -181,4 +181,23 @@ class Oplog(db.Model):
 
 
 if __name__ == "__main__":
-    db.create_all()
+# db.create_all()
+    '''
+    role = Role(
+        name = "超级管理员",
+        auths=""
+    )
+    db.session.add(role)
+    db.session.commit()
+    '''
+
+    from werkzeug.security import generate_password_hash
+
+    admin = Admin(
+        name = "movie",
+        pwd = generate_password_hash("movie"),
+        is_super = 0,
+        role_id = 1
+    )
+    db.session.add(admin)
+    db.session.commit()
