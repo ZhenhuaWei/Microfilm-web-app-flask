3b174232f0568d1b8a2033e44d0de3169621e9df
weizhenhua-deepin <weizhenhua94@163.com>
2019-03-14 21:35:24 +0800

    1. add preview list page.

--- Movie_project/app/admin/views.py
+++ Movie_project/app/admin/views.py
@@ -23,21 +23,31 @@ def pwd():
     return render_template("admin/pwd.html")
 
 
-@admin.route("/tag_add")
+@admin.route("/tag/add")
 def tag_add():
     return render_template("admin/tag_add.html")
 
 
-@admin.route("/tag_list")
+@admin.route("/tag/list")
 def tag_list():
     return render_template("admin/tag_list.html")
 
 
-@admin.route("/movie_add")
+@admin.route("/movie/add")
 def movie_add():
     return render_template("admin/movie_add.html")
 
 
-@admin.route("/movie_list")
+@admin.route("/movie/list")
 def movie_list():
     return render_template("admin/movie_list.html")
+
+
+@admin.route("/preview/add")
+def preview_add():
+    return render_template("admin/preview_add.html")
+
+
+@admin.route("/preview/list")
+def preview_list():
+    return render_template("admin/preview_list.html")
