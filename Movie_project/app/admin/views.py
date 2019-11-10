f14f16dabdee0de918b2f3ee50bd7a4d5dd04452
weizhenhua-deepin <weizhenhua94@163.com>
2019-11-10 15:37:42 +0800

    complate modify admin password function.

--- Movie_project/app/admin/views.py
+++ Movie_project/app/admin/views.py
@@ -2,7 +2,7 @@
 from . import admin
 from flask import render_template, redirect, url_for, flash, session, request
 from functools import wraps
-from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm
+from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm, PwdForm
 from app.models import Admin, Tag, Movie, Preview, User, Comment, Moviecol
 from app import db, app
 from werkzeug.utils import secure_filename
@@ -41,7 +41,7 @@ def login():
         data = form.data
         admin = Admin.query.filter_by(name=data['account']).first()
         if not admin.check_pwd(data['pwd']):
-            flash("密码错误！")
+            flash("密码错误！", "err")
             return redirect(url_for("admin.login"))
         session["admin"] = data["account"]
         return redirect(request.args.get("next") or url_for("admin.index"))
@@ -55,10 +55,21 @@ def logout():
     return redirect(url_for("admin.login"))
 
 
-@admin.route("/pwd")
+# 修改密码
+@admin.route("/pwd", methods=["GET", "POST"])
 @admin_login_req
 def pwd():
-    return render_template("admin/pwd.html")
+    form = PwdForm()
+    if form.validate_on_submit():
+        data = form.data
+        admin = Admin.query.filter_by(name=session["admin"]).first()
+        from werkzeug.security import generate_password_hash
+        admin.pwd = generate_password_hash(data["new_pwd"])
+        db.session.add(admin)
+        db.session.commit()
+        flash("修改密码成功，请重新登录！", "ok")
+        return redirect(url_for('admin.logout'))
+    return render_template("admin/pwd.html", form=form)
 
 
 # 添加标签
@@ -385,6 +396,7 @@ def moviecol_del(id=None):
     db.session.commit()
     flash("删除收藏成功！", "ok")
     return redirect(url_for("admin.moviecol_list", page=1))
+
 
 @admin.route("/oplog/list")
 @admin_login_req
