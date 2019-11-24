ad8ff42dbd746669e567bb323c7a48702c14118c
weizhenhua-deepin <weizhenhua94@163.com>
2019-11-24 21:21:03 +0800

    Close Debug model

--- Movie_project/app/__init__.py
+++ Movie_project/app/__init__.py
@@ -5,12 +5,12 @@ from flask import Flask, render_template
 import pymysql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@127.0.0.1:3306/movie"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@127.0.0.1:3306/movie" #知道数据库用户名 密码 ip和端口
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = 'a99df3b52dca4f3e85f90da2ae86b446'
 app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
 app.config["FC_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/users")
-app.debug = True
+app.debug = False  #True 是开启调试模式 Falsh是关闭调试模式
 db = SQLAlchemy(app)
 
 from app.home import home as home_blueprint
--- Movie_project/manage.py
+++ Movie_project/manage.py
@@ -1,5 +1,7 @@
 # coding:utf8
 from app import app
+from flask_script import Manager #命令行的一个模块
 
+manager = Manager(app)
 if __name__ == "__main__":
     app.run()
